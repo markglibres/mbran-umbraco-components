9db170b48500d9106832d32f4a2697dacda3633d
Mark Gil Libres <markglibres@gmail.com>
2018-01-30 12:25:57 +1100

    add more component samples

---
+++ src/MBran.Components.Test/Pocos/TextAndMediaPoco.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Web.PublishedContentModels;
+
+namespace MBran.Components.Test.Pocos
+{
+    public class TextAndMediaPoco
+    {
+        public string TextMediaTitle { get; set; }
+        public string TextMediaSummary { get; set; }
+        public Image TextMediaImage { get; set; }
+    }
+}
